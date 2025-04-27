02f7f5d9fbe88d1746e8445414669dd79d886bae
Maximiliano Manzur <maximiliano.manzur@uc.cl>
2025-04-27 05:17:33 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { LoginButton, LogoutButton, Profile } from './components/AuthComponents';
-import TestStocks from './testStocks'; // Esta es la vista de listado de stocks, más adelante renombrarás
-import MyPurchases from './pages/MyPurchases'; // Vas a crear esta carpeta y estos archivos
+import Navigation from './components/Navigation';
+import TestStocks from './testStocks';
+import MyPurchases from './pages/MyPurchases';
 import Wallet from './pages/Wallet';
 import StockDetail from './pages/StockDetail';
 import EventLog from './pages/EventLog';
@@ -23,6 +24,7 @@ function App() {
               <Profile />
               <LogoutButton />
             </header>
+            <Navigation />
             <main>
               <Routes>
                 <Route path="/" element={<Navigate to="/stocks" />} />
